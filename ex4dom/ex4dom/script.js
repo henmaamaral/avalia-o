06dc1894b13bf7d5d080f575e49b242fc86b9ed1
Henrique <ricosilvaa3@gmail.com>
2026-03-27 20:02:28 -0300

    Add files via upload

---
+++ ex4dom/ex4dom/script.js
@@ -0,0 +1,9 @@
+const pokemonData = [
+    {nome: 'pokemon1 ',  tipo: 'agua', level: '10'}
+]
+pokemonData.forEach((pkmn) => {
+    
+}
+
+)
+console.log(`O pokemon ${pokemonData[0]} do tipo ${pokemonData[1]}`)
